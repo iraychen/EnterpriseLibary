257a18002970260c61b5a55b30b76620995afdbd
DomoYao <yao-lifeng@163.com>
2014-07-25 18:30:28 +0800

    Add 增加Config 组件

--- Enterprises.Framework.Business/Utility/HttpPostHelper.cs
+++ Enterprises.Framework.Business/Utility/HttpPostHelper.cs
@@ -23,7 +23,7 @@ namespace Enterprises.Framework.Utility
         }
 
         /// <summary>
-        /// 发起post请求
+        /// 原生态发起post请求
         /// </summary>
         /// <param name="data">post数据</param>
         /// <param name="url">post接收地址</param>
@@ -67,5 +67,26 @@ namespace Enterprises.Framework.Utility
             return str;
         }
 
+        /// <summary>
+        /// 利用System.Net WebClient类方法POST DATA
+        /// </summary>
+        /// <returns></returns>
+        public static string Post(string url,string xml)
+        {
+            var client = new WebClient {Encoding = Encoding.UTF8};
+            var fResponse = client.UploadString(url, xml);
+            return fResponse;
+        }
+
+        /// <summary>
+        /// 利用WebClient类方法异步POST DATA
+        /// </summary>
+        /// <returns>异步没有返回值</returns>
+        public static void PostAsync(string url, string xml)
+        {
+            var client = new WebClient { Encoding = Encoding.UTF8 };
+            var uri = new Uri(url);
+            client.UploadStringAsync(uri, xml);
+        }
     }
 }
